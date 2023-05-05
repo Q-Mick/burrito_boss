55cb147c62a765a5da457feae2e4a149e32d3646
Q-Mick <quentinmick@protonmail.com>
2023-05-05 16:38:42 -0600

    inittialize

---
+++ app.js
@@ -0,0 +1,39 @@
+const player =
+{
+  level: 1,
+  money: 0,
+  clickMultiplier: 1,
+  rollingLevel: 1,
+  assistantRoller: 0,
+  
+}
+
+const gameStats =
+{
+  level: 1,
+  timer: 30,
+  demand: 100,
+  multiplier: 1
+}
+
+const upgrades = [
+  {
+    name: "assistant",
+    price: 1000,
+    level: 0,
+  },
+  {
+    name: "tecate",
+    price: 2,
+    quantity: 0,
+  },
+  {
+    name: "chancla",
+    price: 1000,
+    level: 0,
+  },{
+    name: "rolling",
+    price: 1000,
+    level: 0,
+  },
+];
