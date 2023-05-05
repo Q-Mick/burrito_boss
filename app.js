acb3feaa9e5e32be720e53d4f5ab69b3248103a9
Q-Mick <quentinmick@protonmail.com>
2023-05-05 16:53:00 -0600

    initialize

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const player =
 {
   level: 1,
-  money: 0,
+  money: 30,
   clickMultiplier: 1,
-  rollingLevel: 1,
+  rollingSkill: 1,
   assistantRoller: 0,
   
 }
@@ -21,19 +21,44 @@ const upgrades = [
     name: "assistant",
     price: 1000,
     level: 0,
+    isPurchased: false,
   },
   {
     name: "tecate",
     price: 2,
     quantity: 0,
+    isPurchased: false,
   },
   {
     name: "chancla",
     price: 1000,
     level: 0,
+    isPurchased: false,
   },{
     name: "rolling",
     price: 1000,
-    level: 0,
+    level: 1,
+    isPurchased: false,
   },
 ];
+
+const levelUp = [
+  {
+    Image: "assets/burrito_pup.gif"
+  },
+  {
+    Image: "assets/burrito_rub.gif"
+  },
+  {
+    Image: "assets/burrito_iwantaburrito.gif"
+  },
+]
+
+// RANDOM NUMBER FUNCTION
+
+// DRAW FUNCTIONS
+
+// UPGRADE FUNCTIONS
+
+// INTERVAL FUNCTIONS
+
